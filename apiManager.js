307f190cf13900462103c7a6c1e9d39a6fb78fee
KillianVrmr <killianvermeer34@gmail.com>
2024-11-29 11:37:23 +0100

    getting original access to api

---
+++ apiManager.js
@@ -0,0 +1,28 @@
+const class_url = "https://www.dnd5eapi.co/api/classes/";
+const monsters_url = "https://www.dnd5eapi.co/api/monsters/"
+
+async function dndApiConnection(url) {
+    try{
+        const database = await fetch(url);
+        const data = await database.json();
+        console.log(data);
+        return data
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+async function monsterApi(monster) {
+    try{
+        const monsterDatabase = await fetch(`${monsters_url}/${monster}`);
+        const monsterInfo = await monsterDatabase.json();
+        console.log(monsterInfo);
+        return monsterInfo
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+//dndApiConnection(class_url);
+monsterApi("aboleth");
